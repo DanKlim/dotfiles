f16ec25f5d0ed1af6dcd4f0190126d1aa50542af
Roly Fentanes <roly426@gmail.com>
2014-05-11 17:43:07 -0400

    update symlinks

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # bash
-ln -s "$dir"/bash/.bash_profile ~
-ln -s "$dir"/bash/.bashrc ~
-ln -s "$dir"/bash/.alias ~
+ln -sf "$dir"/bash/.bash_profile ~
+ln -sf "$dir"/bash/.bashrc ~
+ln -sf "$dir"/bash/.alias ~
 
 # Only needed if you'll use mongo shell.
-ln -s "$dir"/bash/mongo.js ~
+ln -sf "$dir"/bash/mongo.js ~
 
 # I love vim
-ln -s "$dir"/vim/.vimrc ~
-ln -s "$dir"/vim/.gvimrc ~
-ln -s "$dir"/vim/shared.vim ~/.vim
-ln -s "$dir"/vim/mybundle.vim ~/.vim
+ln -sf "$dir"/vim/.vimrc ~
+ln -sf "$dir"/vim/.gvimrc ~
+ln -sf "$dir"/vim/shared.vim ~/.vim
+ln -sf "$dir"/vim/mybundle.vim ~/.vim
 
 # jshint
-ln -s "$dir"/.jshintrc ~
+ln -sf "$dir"/.jshintrc ~
 
 # keyboard mappings
 # https://pqrs.org/macosx/keyremap4macbook/
-ln -s "$dir"/private.xml ~
-ln -s "$dir"/keyboard ~
-ln -s "$dir"/keyboard/.slate.js ~
-ln -s "$dir"/keyboard/.phoenix ~
-ln -s "$dir"/keyboard/.zephyros.js ~
+ln -sf "$dir"/private.xml ~
+ln -sf "$dir"/keyboard ~
+ln -sf "$dir"/keyboard/.slate.js ~
+ln -sf "$dir"/keyboard/.phoenix ~
+ln -sf "$dir"/keyboard/.zephyros.js ~
